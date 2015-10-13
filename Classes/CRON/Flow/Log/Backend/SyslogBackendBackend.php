4c7347c579f0dd3b24a11af065fae6f78896a8c8
Remus Lazar <remus@lazar.info>
2015-10-13 23:32:23 +0200

    initial commit

---
+++ Classes/CRON/Flow/Log/Backend/SyslogBackendBackend.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lazarrs
+ * Date: 13.10.15
+ * Time: 20:59
+ */
+
+namespace CRON\Flow\Log\Backend;
+
+class SyslogBackend extends \TYPO3\Flow\Log\Backend\AbstractBackend {
+
+	protected $name = 'flow-app';
+
+	/**
+	 * @param string $name
+	 */
+	public function setName($name) {
+		$this->name = $name;
+	}
+
+	protected $facility = LOG_LOCAL1;
+
+	/**
+	 * @param int $facility
+	 */
+	public function setFacility($facility) {
+		$this->facility = $facility;
+	}
+
+	/**
+	 * Carries out all actions necessary to prepare the logging backend, such as opening
+	 * the log file or opening a database connection.
+	 *
+	 * @return void
+	 * @api
+	 */
+	public function open() {
+		openlog($this->name, LOG_PID, $this->facility);
+	}
+
+	/**
+	 * Appends the given message along with the additional information into the log.
+	 *
+	 * @param string $message The message to log
+	 * @param integer $severity One of the LOG_* constants
+	 * @param mixed $additionalData A variable containing more information about the event to be logged
+	 * @param string $packageKey Key of the package triggering the log (determined automatically if not specified)
+	 * @param string $className Name of the class triggering the log (determined automatically if not specified)
+	 * @param string $methodName Name of the method triggering the log (determined automatically if not specified)
+	 * @return void
+	 * @api
+	 */
+	public function append($message, $severity = LOG_INFO, $additionalData = NULL, $packageKey = NULL, $className = NULL, $methodName = NULL) {
+		$output = sprintf('%s %s', $packageKey, $message);
+
+		if (!empty($additionalData)) {
+			$output .= PHP_EOL . $this->getFormattedVarDump($additionalData);
+		}
+
+		syslog($severity, $output);
+	}
+
+	/**
+	 * Carries out all actions necessary to cleanly close the logging backend, such as
+	 * closing the log file or disconnecting from a database.
+	 *
+	 * @return void
+	 * @api
+	 */
+	public function close() {
+		closelog();
+	}
+}
---
+++ README.md
@@ -0,0 +1,39 @@
+Flow Syslog Backend
+===================
+
+Abstract
+--------
+
+This Flow package allows you to use the local syslog daemon for logging and is a replacement for the
+`TYPO3\Flow\Log\Backend\FileBackend`.
+
+This is especially useful while integrating external services for log-management, like e.g.
+[Papertrail](https://papertrailapp.com), which are using the syslog protocol for (securely) access. 
+
+
+Setup
+-----
+
+Configure this Backend for the e.g. systemLogger:
+
+```
+TYPO3:
+  Flow:
+    log:
+      systemLogger:
+        backend: CRON\Flow\Log\Backend\SyslogBackend
+        backendOptions:
+          # identifies the application
+          name: 'my-awesome-flow-app'
+          # syslog facility code, defaults to 17 (LOG_LOCAL1)
+          # see https://en.wikipedia.org/wiki/Syslog
+          facility: 17 
+```
+
+This Backend can be used for other Flow loggers, like `securityLogger`, `sqlLogger`, ...
+
+
+License
+-------
+
+MIT
